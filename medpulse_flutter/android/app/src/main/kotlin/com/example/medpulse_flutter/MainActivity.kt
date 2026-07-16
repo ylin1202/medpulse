f6a5a37b43e919dbfc63f8b9c68c1b76e3ce67d1
ylin1202 <985471zxv@gmail.com>
2026-07-16 22:48:22 +0800

    feat(flutter): initialize project with core dependencies

---
+++ medpulse_flutter/android/app/src/main/kotlin/com/example/medpulse_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medpulse_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ medpulse_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_secure_storage_darwin
+import geolocator_apple
+import package_info_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+}
